40356b7051ebb64775daca2aca71366ac4d9ad13
SamWoog <njdswhy@gmail.com>
2019-09-16 21:57:18 +0800

    KaQLaTor complete

---
+++ Form1.Designer.cs
@@ -0,0 +1,360 @@
+﻿namespace KaQLaTor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.result_label = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.button13 = new System.Windows.Forms.Button();
+            this.button14 = new System.Windows.Forms.Button();
+            this.button15 = new System.Windows.Forms.Button();
+            this.button16 = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.programToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.allClearToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.creditsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // result_label
+            // 
+            this.result_label.Dock = System.Windows.Forms.DockStyle.Top;
+            this.result_label.Font = new System.Drawing.Font("Consolas", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.result_label.Location = new System.Drawing.Point(0, 28);
+            this.result_label.Name = "result_label";
+            this.result_label.Size = new System.Drawing.Size(339, 44);
+            this.result_label.TabIndex = 0;
+            this.result_label.Text = "0.0";
+            this.result_label.TextAlign = System.Drawing.ContentAlignment.BottomRight;
+            this.result_label.UseMnemonic = false;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Consolas", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(12, 75);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(74, 73);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Consolas", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(92, 75);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(74, 73);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Consolas", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(172, 75);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(74, 73);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Consolas", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(252, 75);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(74, 73);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "+";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.Button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Consolas", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(12, 154);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(74, 73);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "4";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.Button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Font = new System.Drawing.Font("Consolas", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button6.Location = new System.Drawing.Point(92, 154);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(74, 73);
+            this.button6.TabIndex = 6;
+            this.button6.Text = "5";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.Button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Font = new System.Drawing.Font("Consolas", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button7.Location = new System.Drawing.Point(172, 154);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(74, 73);
+            this.button7.TabIndex = 7;
+            this.button7.Text = "6";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.Button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Font = new System.Drawing.Font("Consolas", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button8.Location = new System.Drawing.Point(253, 154);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(74, 73);
+            this.button8.TabIndex = 8;
+            this.button8.Text = "-";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.Button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Font = new System.Drawing.Font("Consolas", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button9.Location = new System.Drawing.Point(12, 233);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(74, 73);
+            this.button9.TabIndex = 9;
+            this.button9.Text = "7";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.Button9_Click);
+            // 
+            // button10
+            // 
+            this.button10.Font = new System.Drawing.Font("Consolas", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button10.Location = new System.Drawing.Point(92, 233);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(74, 73);
+            this.button10.TabIndex = 10;
+            this.button10.Text = "8";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.Button10_Click);
+            // 
+            // button11
+            // 
+            this.button11.Font = new System.Drawing.Font("Consolas", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button11.Location = new System.Drawing.Point(172, 233);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(74, 73);
+            this.button11.TabIndex = 11;
+            this.button11.Text = "9";
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.Button11_Click);
+            // 
+            // button12
+            // 
+            this.button12.Font = new System.Drawing.Font("Consolas", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button12.Location = new System.Drawing.Point(253, 233);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(74, 73);
+            this.button12.TabIndex = 12;
+            this.button12.Text = "*";
+            this.button12.UseVisualStyleBackColor = true;
+            this.button12.Click += new System.EventHandler(this.Button12_Click);
+            // 
+            // button13
+            // 
+            this.button13.Font = new System.Drawing.Font("Consolas", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button13.Location = new System.Drawing.Point(12, 312);
+            this.button13.Name = "button13";
+            this.button13.Size = new System.Drawing.Size(74, 73);
+            this.button13.TabIndex = 13;
+            this.button13.Text = ".";
+            this.button13.UseVisualStyleBackColor = true;
+            this.button13.Click += new System.EventHandler(this.Button13_Click);
+            // 
+            // button14
+            // 
+            this.button14.Font = new System.Drawing.Font("Consolas", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button14.Location = new System.Drawing.Point(92, 313);
+            this.button14.Name = "button14";
+            this.button14.Size = new System.Drawing.Size(74, 73);
+            this.button14.TabIndex = 14;
+            this.button14.Text = "0";
+            this.button14.UseVisualStyleBackColor = true;
+            this.button14.Click += new System.EventHandler(this.Button14_Click);
+            // 
+            // button15
+            // 
+            this.button15.Font = new System.Drawing.Font("Consolas", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button15.Location = new System.Drawing.Point(172, 312);
+            this.button15.Name = "button15";
+            this.button15.Size = new System.Drawing.Size(74, 73);
+            this.button15.TabIndex = 15;
+            this.button15.Text = "=";
+            this.button15.UseVisualStyleBackColor = true;
+            this.button15.Click += new System.EventHandler(this.Button15_Click);
+            // 
+            // button16
+            // 
+            this.button16.Font = new System.Drawing.Font("Consolas", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button16.Location = new System.Drawing.Point(253, 313);
+            this.button16.Name = "button16";
+            this.button16.Size = new System.Drawing.Size(74, 73);
+            this.button16.TabIndex = 16;
+            this.button16.Text = "/";
+            this.button16.UseVisualStyleBackColor = true;
+            this.button16.Click += new System.EventHandler(this.Button16_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.programToolStripMenuItem,
+            this.aboutToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(339, 28);
+            this.menuStrip1.TabIndex = 17;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // programToolStripMenuItem
+            // 
+            this.programToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.allClearToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.programToolStripMenuItem.Name = "programToolStripMenuItem";
+            this.programToolStripMenuItem.Size = new System.Drawing.Size(86, 24);
+            this.programToolStripMenuItem.Text = "Program";
+            // 
+            // allClearToolStripMenuItem
+            // 
+            this.allClearToolStripMenuItem.Name = "allClearToolStripMenuItem";
+            this.allClearToolStripMenuItem.Size = new System.Drawing.Size(152, 26);
+            this.allClearToolStripMenuItem.Text = "All Clear";
+            this.allClearToolStripMenuItem.Click += new System.EventHandler(this.AllClearToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(152, 26);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.ExitToolStripMenuItem_Click);
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.creditsToolStripMenuItem});
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(69, 24);
+            this.aboutToolStripMenuItem.Text = "About";
+            // 
+            // creditsToolStripMenuItem
+            // 
+            this.creditsToolStripMenuItem.Name = "creditsToolStripMenuItem";
+            this.creditsToolStripMenuItem.Size = new System.Drawing.Size(144, 26);
+            this.creditsToolStripMenuItem.Text = "Credits";
+            this.creditsToolStripMenuItem.Click += new System.EventHandler(this.CreditsToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(339, 398);
+            this.Controls.Add(this.button16);
+            this.Controls.Add(this.button15);
+            this.Controls.Add(this.button14);
+            this.Controls.Add(this.button13);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.result_label);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "KaQLaTor";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private string pre="";
+        private string preOp = "";
+        private double result=0;
+        private bool use_equal = false;
+        private bool use_dot = false;
+
+        private System.Windows.Forms.Label result_label;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Button button13;
+        private System.Windows.Forms.Button button14;
+        private System.Windows.Forms.Button button15;
+        private System.Windows.Forms.Button button16;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem programToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem allClearToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem creditsToolStripMenuItem;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,329 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KaQLaTor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void CreditsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            new Form2().ShowDialog();
+        }
+
+        private void ExitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void AllClearToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.result = 0;
+            this.pre = "";
+            this.result_label.Text = result.ToString();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            if (this.use_equal)
+            {
+                use_equal = false;
+                this.pre = "";
+            }
+            
+            pre += "1";
+            this.result_label.Text = pre;
+            
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            if (this.use_equal)
+            {
+                use_equal = false;
+                this.pre = "";
+            }
+            pre += "2";
+            this.result_label.Text = pre;
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            if (this.use_equal)
+            {
+                use_equal = false;
+                this.pre = "";
+            }
+            pre += "3";
+            this.result_label.Text = pre;
+        }
+
+        private void Button5_Click(object sender, EventArgs e)
+        {
+            if (this.use_equal)
+            {
+                use_equal = false;
+                this.pre = "";
+            }
+            pre += "4";
+            this.result_label.Text = pre;
+        }
+
+        private void Button6_Click(object sender, EventArgs e)
+        {
+            if (this.use_equal)
+            {
+                use_equal = false;
+                this.pre = "";
+            }
+            pre += "5";
+            this.result_label.Text = pre;
+        }
+
+        private void Button7_Click(object sender, EventArgs e)
+        {
+            if (this.use_equal)
+            {
+                use_equal = false;
+                this.pre = "";
+            }
+            pre += "6";
+            this.result_label.Text = pre;
+        }
+
+        private void Button9_Click(object sender, EventArgs e)
+        {
+            if (this.use_equal)
+            {
+                use_equal = false;
+                this.pre = "";
+            }
+            pre += "7";
+            this.result_label.Text = pre;
+        }
+
+        private void Button10_Click(object sender, EventArgs e)
+        {
+            if (this.use_equal)
+            {
+                use_equal = false;
+                this.pre = "";
+            }
+            pre += "8";
+            this.result_label.Text = pre;
+        }
+
+        private void Button11_Click(object sender, EventArgs e)
+        {
+            if (this.use_equal)
+            {
+                use_equal = false;
+                this.pre = "";
+            }
+            pre += "9";
+            this.result_label.Text = pre;
+        }
+
+        private void Button13_Click(object sender, EventArgs e)
+        {
+            if (this.use_equal)
+            {
+                use_equal = false;
+                this.pre = "";
+            }
+            if (!this.use_dot)
+            {
+                pre += ".";
+                this.use_dot = true;
+                this.result_label.Text = pre;
+            }
+        }
+
+        private void Button14_Click(object sender, EventArgs e)
+        {
+            if (this.use_equal)
+            {
+                use_equal = false;
+                this.pre = "";
+            }
+            if (pre != "")
+            {
+                pre += "0";
+                this.result_label.Text = pre;
+            }
+        }
+
+        private void Button15_Click(object sender, EventArgs e)
+        {
+            //=
+            this.use_equal = true;
+            this.use_dot = false;
+            if (preOp != "") //否则不改变输出
+            {
+                double previous = Double.Parse(this.pre);
+                if (preOp == "+")
+                {
+                    result = result + previous;
+                }
+                if (preOp == "-")
+                {
+                    result = result - previous;
+                }
+                if (preOp == "*")
+                {
+                    result = result * previous;
+                }
+                if (preOp == "/")
+                {
+                    result = result / previous;
+                }
+                this.result_label.Text = result.ToString();
+                this.preOp = "";
+                this.pre = result.ToString();
+            }
+        }
+
+        private void Button4_Click(object sender, EventArgs e)
+        {
+            //+
+            this.use_dot = false;
+            double previous = Double.Parse(this.pre);
+            if (preOp == "")
+            {
+                this.result = previous;
+            }
+            else
+            {
+                if (preOp == "+")
+                {
+                    result = result + previous;
+                }
+                if (preOp == "-")
+                {
+                    result = result - previous;
+                }
+                if (preOp == "*")
+                {
+                    result = result * previous;
+                }
+                if (preOp == "/")
+                {
+                    result = result / previous;
+                }
+            }
+            preOp = "+";
+            this.pre = "";
+            this.result_label.Text = result.ToString();
+        }
+
+        private void Button8_Click(object sender, EventArgs e)
+        {
+            //-
+            this.use_dot = false;
+            double previous = Double.Parse(this.pre);
+            if (preOp == "")
+            {
+                this.result = previous;
+            }
+            else
+            {
+                if (preOp == "+")
+                {
+                    result = result + previous;
+                }
+                if (preOp == "-")
+                {
+                    result = result - previous;
+                }
+                if (preOp == "*")
+                {
+                    result = result * previous;
+                }
+                if (preOp == "/")
+                {
+                    result = result / previous;
+                }
+            }
+            preOp = "-";
+            this.pre = "";
+            this.result_label.Text = result.ToString();
+        }
+
+        private void Button12_Click(object sender, EventArgs e)
+        {
+            //*
+            this.use_dot = false;
+            double previous = Double.Parse(this.pre);
+            if (preOp == "")
+            {
+                this.result = previous;
+            }
+            else
+            {
+                if (preOp == "+")
+                {
+                    result = result + previous;
+                }
+                if (preOp == "-")
+                {
+                    result = result - previous;
+                }
+                if (preOp == "*")
+                {
+                    result = result * previous;
+                }
+                if (preOp == "/")
+                {
+                    result = result / previous;
+                }
+            }
+            preOp = "*";
+            this.pre = "";
+            this.result_label.Text = result.ToString();
+        }
+
+        private void Button16_Click(object sender, EventArgs e)
+        {
+            // /
+            this.use_dot = false;
+            double previous = Double.Parse(this.pre);
+            if (preOp == "")
+            {
+                this.result = previous;
+            }
+            else
+            {
+                if (preOp == "+")
+                {
+                    result = result + previous;
+                }
+                if (preOp == "-")
+                {
+                    result = result - previous;
+                }
+                if (preOp == "*")
+                {
+                    result = result * previous;
+                }
+                if (preOp == "/")
+                {
+                    result = result / previous;
+                }
+            }
+            preOp = "/";
+            this.pre = "";
+            this.result_label.Text = result.ToString();
+        }
+    }
+}
